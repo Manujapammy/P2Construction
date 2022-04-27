7ead88ce01ad1fae0e49687dd6dbb9c043486625
Manujapammy <you@example.com>
2022-04-28 00:22:28 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import './input.css';
+//import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "../src/Navigation/navbar";
+
+function App() {
+  return (
+    <div className="App">
+   <Navbar/>
+   {/* <header className="">  */}
+   <div>
+   <img
+    className=""
+    src="https://images.unsplash.com/photo-1591955506264-3f5a6834570a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGNvbnN0cnVjdGlvbnxlbnwwfHwwfHw%3D&w=1000&q=80"
+    alt=""
+    />
+   </div>
+    {/* </header>  */}
+    </div>
+  );
+}
+
+export default App;
